c4de10d520f2bd988a60e19c630c5702002398d7
Teemu Myllylä <teemu.myllyla@navitep.com>
2020-07-01 14:32:31 +0300

    add frontend build

---
+++ build/precache-manifest.ec810d3606f4c2593556eed3cc291558.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86710b84ff79553ebb94aa9cc83ee9dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db977e8610798d8a546c",
+    "url": "/static/js/2.a64c161a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a64c161a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da93de4cd431f81c5a35",
+    "url": "/static/js/main.4adb6f58.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
