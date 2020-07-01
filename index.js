618d8bf2397d9d2e0fd812237d0775077b01255c
Teemu Myllylä <teemu.myllyla@navitep.com>
2020-07-01 14:38:09 +0300

    serve static

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express()
 
 morgan.token('data', (req, res) => JSON.stringify(req.body))
 
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
