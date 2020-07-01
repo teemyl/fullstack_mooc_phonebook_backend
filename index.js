60b089e295a6dc0b4a060ad0b48c27bab5da94c0
Teemu Myllylä <teemu.myllyla@navitep.com>
2020-07-01 12:04:33 +0300

    part3 3.1

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+let persons = [
+  {
+    id: 0,
+    name: "Teemu Mahanne",
+    number: "040-123123"
+  },
+  {
+    id: 1,
+    name: "John Smith",
+    number: "040-234234"
+  },
+  {
+    id: 2,
+    name: "David Brown",
+    number: "12-23-34-456"
+  },
+  {
+    id: 3,
+    name: "Leeroy Jenkins",
+    number: "024-680-86420"
+  },
+]
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
